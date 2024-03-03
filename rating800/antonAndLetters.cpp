70f626ffa0752162162a8beb72708d2a7a340c2a
Pardarshee Priya <86102820+PardarsheePriya@users.noreply.github.com>
2024-03-03 20:56:34 +0530

    Add files via upload

---
+++ rating800/antonAndLetters.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    unordered_set<char> st;
+    for(char c: s){
+        if(isalpha(c)){
+            st.insert(c);
+        }
+    }
+
+    cout << st.size() << endl;
+
+    return 0;
+}
