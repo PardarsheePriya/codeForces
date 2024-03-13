e17ef1dc632d43089791d56f790aa5eb63ce9aeb
Pardarshee Priya <86102820+PardarsheePriya@users.noreply.github.com>
2024-03-13 21:17:24 +0530

    Reversing Encryption

---
+++ rating900/reverseEncryption.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    for(int i = 2; i <= n; i++){
+        if(n%i == 0){
+            reverse(s.begin(), s.begin()+i);
+        }
+    }
+    
+    cout << s << endl;
+    return 0;
+}
