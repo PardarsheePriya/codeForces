3c827c35c0c2587aca01f61a758d8fb7081b655a
Pardarshee Priya <86102820+PardarsheePriya@users.noreply.github.com>
2024-03-29 13:45:44 +0530

    Sequence Game

---
+++ rating800/sequenceGame.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    
+    vector<ll> b(n);
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    
+    vector<ll> a;
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || b[i] == b[i - 1]) { // If it's the first element or equal to the previous one
+            a.push_back(b[i]);
+        } else if (b[i] < b[i - 1]) { // If it's less than the previous one
+            a.push_back(b[i]);
+            a.push_back(b[i]);
+        } else { // If it's greater than the previous one
+            a.push_back(b[i]);
+        }
+    }
+    
+    cout << a.size() << endl;
+    for (auto it : a) {
+        cout << it << " ";
+    }
+    
+    cout << endl;
+}
+
+int main() {
+    int test;
+    cin >> test;
+    while(test--){
+        solve();
+    }
+    return 0;
+}
