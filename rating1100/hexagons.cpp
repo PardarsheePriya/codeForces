23a93a7051f2dd4cff3c949dd1241208d9aac4ba
Pardarshee Priya <86102820+PardarsheePriya@users.noreply.github.com>
2024-04-05 23:21:54 +0530

    Striver CP 630D - Hexagons!

---
+++ rating1100/hexagons.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+Hexagon
+
+*******************************************************************************/
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#define ll long long int
+
+using namespace std;
+
+void solve(){
+    ll n;
+    cin >> n;
+    ll ans = 3*n*(n+1) + 1;
+    cout << ans << endl;
+    
+}
+int main()
+{solve();
+}
